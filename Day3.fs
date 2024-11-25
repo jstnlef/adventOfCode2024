dee3c4bf8c00d6f8fdc5d00eb9de92d15c75d84d
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-11-25 12:47:10 -0500

    stub out some more files

--- Day3.fs
+++ Day3.fs
@@ -1,16 +1,16 @@
-﻿module Day2
+﻿module Day3
 
 module Tests =
   open Xunit
 
   [<Theory>]
-  [<InlineData("Inputs/Day2/test.txt", -1)>]
-  [<InlineData("Inputs/Day2/input.txt", -1)>]
+  [<InlineData("Inputs/Day3/test.txt", -1)>]
+  [<InlineData("Inputs/Day3/input.txt", -1)>]
   let ``Part 1`` (filename: string, expected: int) =
     Assert.True(false)
 
   [<Theory>]
-  [<InlineData("Inputs/Day2/test.txt", -1)>]
-  [<InlineData("Inputs/Day2/input.txt", -1)>]
+  [<InlineData("Inputs/Day3/test.txt", -1)>]
+  [<InlineData("Inputs/Day3/input.txt", -1)>]
   let ``Part 2`` (filename: string, expected: int) =
     Assert.True(false)
--- Day4.fs
+++ Day4.fs
@@ -1,16 +1,16 @@
-﻿module Day2
+﻿module Day4
 
 module Tests =
   open Xunit
 
   [<Theory>]
-  [<InlineData("Inputs/Day2/test.txt", -1)>]
-  [<InlineData("Inputs/Day2/input.txt", -1)>]
+  [<InlineData("Inputs/Day4/test.txt", -1)>]
+  [<InlineData("Inputs/Day4/input.txt", -1)>]
   let ``Part 1`` (filename: string, expected: int) =
     Assert.True(false)
 
   [<Theory>]
-  [<InlineData("Inputs/Day2/test.txt", -1)>]
-  [<InlineData("Inputs/Day2/input.txt", -1)>]
+  [<InlineData("Inputs/Day4/test.txt", -1)>]
+  [<InlineData("Inputs/Day4/input.txt", -1)>]
   let ``Part 2`` (filename: string, expected: int) =
     Assert.True(false)
