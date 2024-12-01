2ec3e546b636140f68ccffd2a7cf554346d44ca8
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-01 00:36:12 -0500

    day 1 part one complete

--- Day1.fs
+++ Day1.fs
@@ -1,16 +1,41 @@
 ﻿module Day1
 
+open adventOfCode2024.Common
+
+type LocationId = int
+
+type Locations =
+  { a: LocationId array
+    b: LocationId array }
+
+module Locations =
+  let sumOfSmallestDistances locations =
+    let a = locations.a |> Array.sort
+    let b = locations.b |> Array.sort
+    Array.zip a b |> Array.sumBy (fun (a, b) -> abs (a - b))
+
+  let parse filename : Locations =
+    let a, b =
+      filename
+      |> Input.parseByLine (fun s ->
+        let split = s.Split("   ")
+        int (split[0]), int (split[1]))
+      |> Seq.toArray
+      |> Array.unzip
+
+    { a = a; b = b }
+
 module Tests =
   open Xunit
 
   [<Theory>]
-  [<InlineData("Inputs/Day1/test.txt", -1)>]
-  [<InlineData("Inputs/Day1/input.txt", -1)>]
-  let ``Part 1`` (filename: string, expected: int) =
-    Assert.True(false)
+  [<InlineData("Inputs/Day1/test.txt", 11)>]
+  [<InlineData("Inputs/Day1/input.txt", 2000468)>]
+  let ``Sum of smallest distances between location ids`` (filename: string, expected: int) =
+    let result = filename |> Locations.parse |> Locations.sumOfSmallestDistances
+    Assert.Equal(expected, result)
 
   [<Theory>]
   [<InlineData("Inputs/Day1/test.txt", -1)>]
   [<InlineData("Inputs/Day1/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) =
-    Assert.True(false)
+  let ``Part 2`` (filename: string, expected: int) = Assert.True(false)
