1189c1b39675258a13d05b47898f70f44616a0ea
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-02 01:24:18 -0500

    day 2 part 2 complete

--- Day2.fs
+++ Day2.fs
@@ -6,21 +6,24 @@ let parseReports filename =
   filename |> Input.parseByLine (fun line -> line.Split(" ") |> Array.map int)
 
 module Report =
-  let private compareLevels state (a, b) =
+  let isWithinTolerances acc delta =
+    (not (abs delta = 0 || abs delta > 3))
+    && (acc <= 0 && delta < 0 || acc >= 0 && delta > 0)
+
+  let compareRawLevels state (a, b) =
     match state with
     | acc, false -> (acc, false)
     | acc, true ->
       let delta = a - b
-
-      if (abs delta = 0 || abs delta > 3) then
-        (acc, false)
-      elif (acc <= 0 && delta < 0) || (acc >= 0 && delta > 0) then
-        (acc + delta, true)
-      else
-        (acc, false)
+      (acc + delta, isWithinTolerances acc delta)
 
   let isSafe report =
-    report |> Array.pairwise |> Array.fold compareLevels (0, true) |> snd
+    report |> Array.pairwise |> Array.fold compareRawLevels (0, true) |> snd
+
+  let isSafeWithProblemDampener (report: int array) =
+    seq { 0 .. report.Length - 1 }
+    |> Seq.exists (fun i -> report |> Array.removeAt i |> isSafe)
+
 
 module Tests =
   open Xunit
@@ -33,6 +36,13 @@ module Tests =
     Assert.Equal(expected, result)
 
   [<Theory>]
-  [<InlineData("Inputs/Day2/test.txt", -1)>]
-  [<InlineData("Inputs/Day2/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) = Assert.True(false)
+  [<InlineData("Inputs/Day2/test.txt", 4)>]
+  [<InlineData("Inputs/Day2/input.txt", 373)>]
+  let ``Part 2: Number of safe reactor reports with problem dampener`` (filename: string, expected: int) =
+    let result =
+      filename
+      |> parseReports
+      |> Seq.filter Report.isSafeWithProblemDampener
+      |> Seq.length
+
+    Assert.Equal(expected, result)
