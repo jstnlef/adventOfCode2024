2a4cb301271051d7b521372de4db9a5041b4c95b
Justin <jstnlefebvre@gmail.com>
2024-12-04 11:05:09 -0500

    finished day4 part 1 and 2

--- Day4.fs
+++ Day4.fs
@@ -16,15 +16,16 @@ let parseToCharMap filename =
 
   d
 
-let allDirections x =
-  [| for y in -1 .. 1 do
+let possibleXMASPositions (x, y) : (int * int) array seq =
+  let allDirections =
+    [| for y in -1 .. 1 do
        for x in -1 .. 1 do
          if (x, y) <> (0, 0) then
            yield (x, y) |]
-
-let possibleWordPositions (x, y) : (int * int) array seq =
-  let dirs = allDirections 1
-  seq { [| (5, 0); (6, 0); (7, 0); (8, 0) |] }
+  seq {
+    for dx, dy in allDirections do
+      yield Array.init 4 (fun i -> x+dx*i, y+dy*i)
+  }
 
 let wordIsXMAS (charMap: Dictionary<char, Set<int * int>>) positions =
   let word = "XMAS"
@@ -36,8 +37,28 @@ let wordIsXMAS (charMap: Dictionary<char, Set<int * int>>) positions =
 
 let countXMAS (charMap: Dictionary<char, Set<int * int>>) =
   charMap['X']
-  |> Seq.collect possibleWordPositions
+  |> Seq.collect possibleXMASPositions
   |> Seq.filter (wordIsXMAS charMap)
+  |> Seq.length
+
+let isCrossMAS (charMap: Dictionary<char, Set<int * int>>) (x, y) =
+  let allPositions =
+    [| (-1, -1); (-1, 1); (1, 1); (1, -1) |]
+    |> Array.map (fun (dx, dy) -> x + dx, y + dy)
+
+  let checkIfCross p1 p2 =
+    if charMap['M'].Contains(p1) then
+      charMap['S'].Contains(p2)
+    elif charMap['S'].Contains(p1) then
+      charMap['M'].Contains(p2)
+    else
+      false
+
+  checkIfCross allPositions[0] allPositions[2] && checkIfCross allPositions[1] allPositions[3]
+
+let countCrossMAS (charMap: Dictionary<char, Set<int * int>>) =
+  charMap['A']
+  |> Seq.filter (isCrossMAS charMap)
   |> Seq.length
 
 module Tests =
@@ -45,12 +66,14 @@ module Tests =
 
   [<Theory>]
   [<InlineData("Inputs/Day4/test.txt", 18)>]
-  [<InlineData("Inputs/Day4/input.txt", -1)>]
+  [<InlineData("Inputs/Day4/input.txt", 2397)>]
   let ``Part 1: Number of times XMAS appears`` (filename: string, expected: int) =
     let result = filename |> parseToCharMap |> countXMAS
     Assert.Equal(expected, result)
 
   [<Theory>]
-  [<InlineData("Inputs/Day4/test.txt", -1)>]
-  [<InlineData("Inputs/Day4/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) = Assert.True(false)
+  [<InlineData("Inputs/Day4/test.txt", 9)>]
+  [<InlineData("Inputs/Day4/input.txt", 1824)>]
+  let ``Part 2: Number of times X-MAS appears`` (filename: string, expected: int) =
+    let result = filename |> parseToCharMap |> countCrossMAS
+    Assert.Equal(expected, result)
