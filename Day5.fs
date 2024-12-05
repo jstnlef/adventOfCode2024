14b9a5f3ec08887074999d14f78c03e01f366471
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-05 01:03:02 -0500

    day 5 part 1 complete

--- Day5.fs
+++ Day5.fs
@@ -3,20 +3,18 @@ module Day5
 open System.IO
 
 type State =
-  { rules: Map<int, int>
+  { rules: (int * int) array
     updates: int array array }
 
 let parse filename : State =
   let text = filename |> File.ReadAllText
   let split = text.Split("\n\n")
 
-  // TODO: THIS IS WRONG!
   let rules =
     split[0].Split("\n")
     |> Array.map (fun s ->
       let split = s.Split("|")
       int split[0], int split[1])
-    |> Map
 
   let updates =
     split[1].Trim().Split("\n")
@@ -24,16 +22,27 @@ let parse filename : State =
 
   { rules = rules; updates = updates }
 
-let findCorrectUpdates state : int array seq = state.updates
+let findCorrectUpdates state : int array array =
+  let isUpdateCorrect update =
+    let indexMap = update |> Array.mapi (fun idx value -> value, idx) |> Map.ofArray
 
-let findMiddle (update: int array) = update[update.Length / 2 + 1]
+    let verifyRule (a, b) =
+      match Map.tryFind a indexMap, Map.tryFind b indexMap with
+      | Some(i), Some(j) -> i < j
+      | _ -> true
+
+    state.rules |> Array.forall verifyRule
+
+  state.updates |> Array.filter isUpdateCorrect
+
+let findMiddle (update: int array) = update[update.Length / 2]
 
 module Tests =
   open Xunit
 
   [<Theory>]
   [<InlineData("Inputs/Day5/test.txt", 143)>]
-  [<InlineData("Inputs/Day5/input.txt", -1)>]
+  [<InlineData("Inputs/Day5/input.txt", 4924)>]
   let ``Part 1: Sum of middle pages of correct updates to safety manual`` (filename: string, expected: int) =
     let result = filename |> parse |> findCorrectUpdates |> Seq.sumBy findMiddle
     Assert.Equal(expected, result)
