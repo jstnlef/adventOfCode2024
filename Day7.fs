1cd22a123e10fd4b299d5c6687d7a467ae9537b6
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-07 16:52:28 -0500

    day 7 performance fixes

--- Day7.fs
+++ Day7.fs
@@ -2,29 +2,27 @@ module Day7
 
 open System.IO
 
-type Equation = { test: int64; operands: int64 array }
+type Equation = { test: int64; operands: int64 list }
 
 let canBeSolved operators equation =
-  let rec doSolve acc (remaining: int64 array) =
-    if Array.isEmpty remaining then
-      equation.test = acc
-    elif acc > equation.test then
+  let rec isSolved acc remaining =
+    if acc > equation.test then
       false
     else
-      operators
-      |> Array.exists (fun op -> doSolve (op acc remaining[0]) remaining[1..])
+      match remaining with
+      | [] -> acc = equation.test
+      | head :: tail -> operators |> Array.exists (fun op -> isSolved (op acc head) tail)
 
-  doSolve 0 equation.operands
-
+  isSolved 0 equation.operands
 
 let parse filename =
   let parseLine (line: string) =
-    let split = line.Split(":")
+    let split = line.Split(": ")
 
     { test = int64 split[0]
-      operands = split[1].Trim().Split(" ") |> Array.map int64 }
+      operands = split[1].Split(" ") |> Array.map int64 |> Array.toList }
 
-  filename |> File.ReadLines |> Seq.map parseLine |> Seq.toArray
+  filename |> File.ReadLines |> Seq.map parseLine
 
 module Tests =
   open Xunit
@@ -34,21 +32,18 @@ module Tests =
   [<InlineData("Inputs/Day7/input.txt", 1545311493300L)>]
   let ``Part 1: Total bridge calibrations`` (filename: string, expected: int64) =
     let result =
-      filename
-      |> parse
-      |> Array.filter (canBeSolved [| (+); (*) |])
-      |> Array.sumBy _.test
+      filename |> parse |> Seq.filter (canBeSolved [| (+); (*) |]) |> Seq.sumBy _.test
 
     Assert.Equal(expected, result)
 
   [<Theory>]
   [<InlineData("Inputs/Day7/test.txt", 11387)>]
   [<InlineData("Inputs/Day7/input.txt", 169122112716571L)>]
-  let ``Part 2`` (filename: string, expected: int64) =
+  let ``Part 2: Total bridge calibrations with concat`` (filename: string, expected: int64) =
     let result =
       filename
       |> parse
-      |> Array.filter (canBeSolved [| (+); (*); (fun a b -> int64 $"{a}{b}") |])
-      |> Array.sumBy _.test
+      |> Seq.filter (canBeSolved [| (+); (*); (fun a b -> int64 $"{a}{b}") |])
+      |> Seq.sumBy _.test
 
     Assert.Equal(expected, result)
