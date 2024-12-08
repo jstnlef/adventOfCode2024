6f78511b6172a951dd0388631d6cca43b3cb2446
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-07 20:11:50 -0500

    Finished day6 part 2 finally

--- Day6.fs
+++ Day6.fs
@@ -10,7 +10,9 @@ type Guard =
 type Lab =
   { map: string array
     guard: Guard
-    visited: Set<int * int> }
+    visited: Set<Guard>
+    hasLoop: bool
+    terminal: bool }
 
 module Guard =
   let lookAhead guard : int * int =
@@ -24,40 +26,40 @@ module Guard =
     { guard with
         position = lookAhead guard }
 
-let isInbounds (x, y) mapState =
-  let height = mapState.map.Length
-  let width = mapState.map[0].Length
-  x >= 0 && x < width && y >= 0 && y < height
+let simulateGuardPatrol (newObstacle: Option<int * int>) initial =
+  let isInbounds (x, y) =
+    let height = initial.map.Length
+    let width = initial.map[0].Length
+    x >= 0 && x < width && y >= 0 && y < height
 
-let stateIsValid mapState =
-  isInbounds mapState.guard.position mapState
+  let willHitNewObstacle (x, y) =
+    match newObstacle with
+    | Some(obX, obY) -> (x, y) = (obX, obY)
+    | None -> false
 
-let countGuardMovement initialMap =
-  let moveGuard mapState _ =
-    let x, y = Guard.lookAhead mapState.guard
+  let moveGuard lab =
+    let x, y = Guard.lookAhead lab.guard
 
-    if mapState |> (isInbounds (x, y) >> not) then
-      // I still want to move the guard out of bounds so the outer loop
-      // terminates
-      { mapState with
-          guard = mapState.guard |> Guard.move }
+    if lab.hasLoop || not (isInbounds (x, y)) then
+      { lab with terminal = true }
     else
       let newGuard =
-        if mapState.map[y][x] = '#' then
-          mapState.guard |> (Guard.rotate >> Guard.move)
+        if lab.map[y][x] = '#' || willHitNewObstacle (x, y) then
+          lab.guard |> Guard.rotate
         else
-          mapState.guard |> Guard.move
+          lab.guard |> Guard.move
 
-      { mapState with
+      let hasLoop = Set.contains newGuard lab.visited
+
+      { lab with
           guard = newGuard
-          visited = Set.add newGuard.position mapState.visited }
+          visited = Set.add newGuard lab.visited
+          hasLoop = hasLoop }
 
   Seq.initInfinite id
-  |> Seq.scan moveGuard initialMap
-  |> Seq.takeWhile stateIsValid
+  |> Seq.scan (fun state _ -> moveGuard state) initial
+  |> Seq.takeWhile (_.terminal >> not)
   |> Seq.last
-  |> _.visited
-  |> Seq.length
 
 let parseMap filename : Lab =
   let lines = filename |> File.ReadLines |> Seq.toArray
@@ -70,23 +72,45 @@ let parseMap filename : Lab =
     }
     |> Seq.find (fun (_, _, c) -> c = '^')
 
+  let guard = { position = x, y; direction = 0, -1 }
+
   { map = lines
-    guard = { position = x, y; direction = 0, -1 }
-    visited = Set [ x, y ] }
+    guard = guard
+    visited = Set [ guard ]
+    hasLoop = false
+    terminal = false }
 
 module Tests =
   open Xunit
 
   [<Theory>]
   [<InlineData("Inputs/Day6/test.txt", 41)>]
+  [<InlineData("Inputs/Day6/test2.txt", 91)>]
   [<InlineData("Inputs/Day6/input.txt", 4602)>]
   let ``Part 1: Number of spots visited by the guard`` (filename: string, expected: int) =
-    let result = filename |> parseMap |> countGuardMovement
+    let result =
+      filename
+      |> parseMap
+      |> simulateGuardPatrol None
+      |> _.visited
+      |> Set.map _.position
+      |> Set.count
+
     Assert.Equal(expected, result)
 
   [<Theory>]
   [<InlineData("Inputs/Day6/test.txt", 6)>]
-  [<InlineData("Inputs/Day6/input.txt", -1)>]
+  [<InlineData("Inputs/Day6/test2.txt", 19)>]
+  [<InlineData("Inputs/Day6/input.txt", 1703)>]
   let ``Part 2: Number of possible obstructions for guard loops`` (filename: string, expected: int) =
-    let result = filename |> parseMap |> (fun _ -> 0)
+    let initial = filename |> parseMap
+
+    let result =
+      initial
+      |> simulateGuardPatrol None
+      |> _.visited
+      |> Set.map _.position
+      |> Seq.filter (fun p -> p <> initial.guard.position && simulateGuardPatrol (Some p) initial |> _.hasLoop)
+      |> Seq.length
+
     Assert.Equal(expected, result)
