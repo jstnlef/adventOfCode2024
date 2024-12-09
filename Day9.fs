688eab120a0f5eac4032e61b82f7d6838e591b60
Justin <jstnlefebvre@gmail.com>
2024-12-09 17:05:36 -0500

    wip day 9 part 2

--- Day9.fs
+++ Day9.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 
 type Disk = int16 array
+type File = int16 * int * int
+type Free = int * int
 
 module Disk =
   let checksum (disk: Disk) : int64 =
@@ -11,28 +13,54 @@ module Disk =
     |> Array.indexed
     |> Array.sumBy (fun (i, id) -> if id > 0s then int64 i * int64 id else 0)
 
-  let swap i j (disk: Disk) =
-    let temp = disk[i]
+  let moveBlock i j (disk: Disk) =
     disk[i] <- disk[j]
-    disk[j] <- temp
+    disk[j] <- -1s
 
   let blockDefragment (disk: Disk) =
     let mutable frontI = 0
     let mutable backI = disk.Length - 1
 
-    while frontI <> backI do
-      let mutable frontV = disk[frontI]
-      let mutable backV = disk[backI]
-
-      if frontV >= 0s then frontI <- frontI + 1
-      elif backV < 0s then backI <- backI - 1
-      else swap frontI backI disk
+    while frontI < backI do
+      if disk[frontI] >= 0s then frontI <- frontI + 1
+      elif disk[backI] < 0s then backI <- backI - 1
+      else moveBlock frontI backI disk
 
     disk
 
-  let fileDefragment (disk: Disk) = disk
+  let moveFile (_, sourceI, size) targetI (disk: Disk) =
+    for n in 0 .. (size - 1) do
+      moveBlock (sourceI + n) (targetI + n) disk
 
-let parseDiskMap filename : Disk =
+  let fileDefragment (files: File list, freeSpace: Free array, disk: Disk) =
+    let mutable free = freeSpace
+
+    let findAndMarkOpenSpace (_, _, size) : int option =
+      let maybeOpen =
+        freeSpace
+        |> Seq.indexed
+        |> Seq.tryFind (fun (_, (_, freeSize)) -> size <= freeSize)
+
+      match maybeOpen with
+      | Some(i, (freeI, freeSize)) ->
+        if size < freeSize then
+          free[i] <- (freeI + size, freeSize - size)
+        elif size = freeSize then
+          free <- Array.removeAt i free
+        else
+          ()
+
+        Some freeI
+      | None -> None
+
+    for file in files do
+      match findAndMarkOpenSpace file with
+      | Some i -> moveFile file i disk
+      | None -> ()
+
+    disk
+
+let parseDiskMap filename =
   let diskInput =
     filename
     |> File.ReadAllText
@@ -44,16 +72,22 @@ let parseDiskMap filename : Disk =
   let mutable i = 0
   let mutable fileId = 0s
 
+  let mutable files = []
+  let mutable free = []
+
   for inputIndex, segmentSize in diskInput |> Seq.indexed do
     if inputIndex % 2 = 0 then
       for j in 0 .. (segmentSize - 1) do
         disk[i + j] <- fileId
 
+      files <- (fileId, i, segmentSize) :: files
       fileId <- fileId + 1s
+    else if segmentSize > 0 then
+      free <- (i, segmentSize) :: free
 
     i <- i + segmentSize
 
-  disk
+  files, free |> List.rev |> List.toArray, disk
 
 module Tests =
   open Xunit
@@ -62,12 +96,17 @@ module Tests =
   [<InlineData("Inputs/Day9/test.txt", 1928)>]
   [<InlineData("Inputs/Day9/input.txt", 6291146824486L)>]
   let ``Part 1: Checksum after block defragmentation`` (filename: string, expected: int64) =
-    let result = filename |> parseDiskMap |> Disk.blockDefragment |> Disk.checksum
+    let result =
+      filename
+      |> parseDiskMap
+      |> (fun (_, _, disk) -> Disk.blockDefragment disk)
+      |> Disk.checksum
+
     Assert.Equal(expected, result)
 
   [<Theory>]
   [<InlineData("Inputs/Day9/test.txt", 2858)>]
   [<InlineData("Inputs/Day9/input.txt", -1)>]
-  let ``Part 2: Checksum after file deefragmentation`` (filename: string, expected: int64) =
+  let ``Part 2: Checksum after file defragmentation`` (filename: string, expected: int64) =
     let result = filename |> parseDiskMap |> Disk.fileDefragment |> Disk.checksum
     Assert.Equal(expected, result)
