ba807030248f1ff527942290e7d8119617290a69
Justin <jstnlefebvre@gmail.com>
2024-12-11 13:22:29 -0500

    day 11 part 2 complete

--- Common/Functools.fs
+++ Common/Functools.fs
@@ -1,18 +1,41 @@
 module Common.Functools
 
-open System.Collections.Generic
+open System.Collections.Concurrent
 
 let uncurry f (a, b) = f a b
 
-let memoize f =
-  let dict = Dictionary<_, _>()
+let memoize (f: 'a -> 'b) =
+  let cache = ConcurrentDictionary<'a, Lazy<'b>>()
+  fun x -> cache.GetOrAdd(x, lazy (f x)).Value
 
-  fun c ->
-    let exist, value = dict.TryGetValue c
+let memoize2 (f: 'a -> 'b -> 'c) =
+  let f' = memoize (fun (x, y) -> f x y)
+  fun x y -> f' (x, y)
 
-    match exist with
-    | true -> value
-    | _ ->
-      let value = f c
-      dict.Add(c, value)
-      value
+let memoize3 (f: 'a -> 'b -> 'c -> 'd) =
+  let f' = memoize (fun (x, y, z) -> f x y z)
+  fun x y z -> f' (x, y, z)
+
+let memoizeRec f =
+  let cache = ConcurrentDictionary<_, Lazy<_>>()
+
+  let rec recF x =
+    cache.GetOrAdd(x, lazy (f recF x)).Value
+
+  recF
+
+let memoizeRec2 f =
+  let cache = ConcurrentDictionary<_, Lazy<_>>()
+
+  let rec recF x y =
+    cache.GetOrAdd((x, y), lazy (f recF x y)).Value
+
+  recF
+
+let memoizeRec3 f =
+  let cache = ConcurrentDictionary<_, Lazy<_>>()
+
+  let rec recF x y z =
+    cache.GetOrAdd((x, y, z), lazy (f recF x y z)).Value
+
+  recF
--- Day11.fs
+++ Day11.fs
@@ -2,20 +2,26 @@ module Day11
 
 open System
 open System.IO
+open Common
 
-let blink numBlinks stones : int64 array =
-  let transform stone =
-    if stone = 0L then
-      [| 1L |]
-    elif (string stone).Length % 2 = 0 then
-      let s = (string stone)
-      let midpoint = s.Length / 2
-      [| Int64.Parse(s[.. midpoint - 1]); Int64.Parse(s[midpoint..]) |]
-    else
-      [| stone * 2024L |]
+let blink numBlinks stones =
+  let countStoneGeneration =
+    Functools.memoizeRec2 (fun countStoneGeneration blinks stone ->
+      if blinks = 0 then
+        1L
+      elif stone = 0L then
+        countStoneGeneration (blinks - 1) 1
+      elif (string stone).Length % 2 = 0 then
+        let s = (string stone)
+        let midpoint = s.Length / 2
+        let l = Int64.Parse(s[.. midpoint - 1])
+        let r = Int64.Parse(s[midpoint..])
 
-  seq { 0 .. numBlinks - 1 }
-  |> Seq.fold (fun stones _ -> stones |> Array.collect transform) stones
+        (countStoneGeneration (blinks - 1) l) + (countStoneGeneration (blinks - 1) r)
+      else
+        countStoneGeneration (blinks - 1) (stone * 2024L))
+
+  stones |> Array.sumBy (countStoneGeneration numBlinks)
 
 let parse filename =
   filename |> File.ReadAllText |> _.Split(" ") |> Array.map Int64.Parse
@@ -26,13 +32,13 @@ module Tests =
   [<Theory>]
   [<InlineData("Inputs/Day11/test.txt", 55312)>]
   [<InlineData("Inputs/Day11/input.txt", 191690)>]
-  let ``Part 1: Number of mysterious stones after 25 blinks`` (filename: string, expected: int) =
-    let result = filename |> parse |> blink 25 |> Array.length
+  let ``Part 1: Number of mysterious stones after 25 blinks`` (filename: string, expected: int64) =
+    let result = filename |> parse |> blink 25
     Assert.Equal(expected, result)
 
   [<Theory>]
-  [<InlineData("Inputs/Day11/test.txt", -1)>]
-  [<InlineData("Inputs/Day11/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) =
-    let result = filename |> parse |> blink 75 |> Array.length
+  [<InlineData("Inputs/Day11/test.txt", 65601038650482L)>]
+  [<InlineData("Inputs/Day11/input.txt", 228651922369703L)>]
+  let ``Part 2: Number of mysterious stones after 75 blinks`` (filename: string, expected: int64) =
+    let result = filename |> parse |> blink 75
     Assert.Equal(expected, result)
