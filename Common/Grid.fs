82091dd278c6031ed4b212314f26f6ad0fc54288
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-10 21:03:14 -0500

    refactoring!

--- Common/Grid.fs
+++ Common/Grid.fs
@@ -14,13 +14,14 @@ let inbounds width height (x, y) =
 
 let get (grid: Grid<'a>) (x, y) = grid[y][x]
 
-let private neighbors vectors width height (x, y) =
+let neighborsNoGrid vectors width height (x, y) =
   vectors
   |> Array.map (fun (dx, dy) -> x + dx, y + dy)
   |> Array.filter (inbounds width height)
 
-let cardinalNeighbors (grid: Grid<'a>) position =
-  neighbors cardinalVectors grid[0].Length grid.Length position
+let neighbors vectors (grid: Grid<'a>) position =
+  neighborsNoGrid vectors grid[0].Length grid.Length position
 
-let eightWayNeighbors (grid: Grid<'a>) position =
-  neighbors eightWayVectors grid[0].Length grid.Length position
+let cardinalNeighbors (grid: Grid<'a>) position = neighbors cardinalVectors grid position
+
+let eightWayNeighbors (grid: Grid<'a>) position = neighbors eightWayVectors grid position
