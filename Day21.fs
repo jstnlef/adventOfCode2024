2262c44e7fb80aca1829218cb79a9c8500c04ccf
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-21 23:19:18 -0500

    day 21 stub

---
+++ Day21.fs
@@ -0,0 +1,18 @@
+module Day21
+
+module Tests =
+  open Xunit
+
+  [<Theory>]
+  [<InlineData("Inputs/Day21/test.txt", -1)>]
+  [<InlineData("Inputs/Day21/input.txt", -1)>]
+  let ``Part 1: Sum of the complexities of the 5 codes`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
+
+  [<Theory>]
+  [<InlineData("Inputs/Day21/test.txt", -1)>]
+  [<InlineData("Inputs/Day21/input.txt", -1)>]
+  let ``Part 2`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
