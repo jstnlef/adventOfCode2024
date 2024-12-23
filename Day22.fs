8a9f3cc3d9a8e9694639d3ebda9d5a329a7d6f0f
Justin <jstnlefebvre@gmail.com>
2024-12-23 13:46:46 -0500

    stub day 22 and 23

---
+++ Day22.fs
@@ -0,0 +1,18 @@
+module Day22
+
+module Tests =
+  open Xunit
+
+  [<Theory>]
+  [<InlineData("Inputs/Day22/test.txt", -1)>]
+  [<InlineData("Inputs/Day22/input.txt", -1)>]
+  let ``Part 1`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
+
+  [<Theory>]
+  [<InlineData("Inputs/Day22/test.txt", -1)>]
+  [<InlineData("Inputs/Day22/input.txt", -1)>]
+  let ``Part 2`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
---
+++ Day23.fs
@@ -0,0 +1,18 @@
+module Day23
+
+module Tests =
+  open Xunit
+
+  [<Theory>]
+  [<InlineData("Inputs/Day23/test.txt", -1)>]
+  [<InlineData("Inputs/Day23/input.txt", -1)>]
+  let ``Part 1`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
+
+  [<Theory>]
+  [<InlineData("Inputs/Day23/test.txt", -1)>]
+  [<InlineData("Inputs/Day23/input.txt", -1)>]
+  let ``Part 2`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
