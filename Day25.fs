959efae695b64448067afe8da75ec527330275b7
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-25 01:25:43 -0500

    stub out the last day!

---
+++ Day25.fs
@@ -0,0 +1,18 @@
+module Day25
+
+module Tests =
+  open Xunit
+
+  [<Theory>]
+  [<InlineData("Inputs/Day25/test.txt", -1)>]
+  [<InlineData("Inputs/Day25/input.txt", -1)>]
+  let ``Part 1`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
+
+  [<Theory>]
+  [<InlineData("Inputs/Day25/test.txt", -1)>]
+  [<InlineData("Inputs/Day25/input.txt", -1)>]
+  let ``Part 2`` (filename: string, expected: int) =
+    let result = 0
+    Assert.Equal(expected, result)
