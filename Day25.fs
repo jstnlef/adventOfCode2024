f2b7d527b44ce2444d182d46a677ce67396f5046
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-25 12:15:33 -0500

    day 25 part 1

--- Day25.fs
+++ Day25.fs
@@ -1,13 +1,50 @@
 module Day25
 
+open System.IO
+open Common
+
+let numberOfKeyLockCombinations keys locks =
+  let mutable n = 0
+
+  for key in keys do
+    for lock in locks do
+      if Array.zip key lock |> Array.forall (fun (k, l) -> k + l < 6) then
+        n <- n + 1
+
+  n
+
+let parse filename =
+  let mutable keys = []
+  let mutable locks = []
+
+  let parseKeyOrLock (keyOrLock: string) =
+    let s = keyOrLock.Split("\n") |> array2D
+
+    let heights =
+      seq { 0 .. s[0, *].Length - 1 }
+      |> Seq.map (fun i -> s[*, i] |> Array.filter (fun c -> c = '#') |> (fun c -> Array.length c - 1))
+      |> Seq.toArray
+
+    if s[0, 0] = '#' then
+      locks <- heights :: locks
+    else
+      keys <- heights :: keys
+
+  filename
+  |> File.ReadAllText
+  |> _.Trim().Split("\n\n")
+  |> Array.iter parseKeyOrLock
+
+  keys, locks
+
 module Tests =
   open Xunit
 
   [<Theory>]
-  [<InlineData("Inputs/Day25/test.txt", -1)>]
-  [<InlineData("Inputs/Day25/input.txt", -1)>]
-  let ``Part 1`` (filename: string, expected: int) =
-    let result = 0
+  [<InlineData("Inputs/Day25/test.txt", 3)>]
+  [<InlineData("Inputs/Day25/input.txt", 3508)>]
+  let ``Part 1: Number of unique key and lock combinations`` (filename: string, expected: int) =
+    let result = filename |> parse |> Functools.uncurry numberOfKeyLockCombinations
     Assert.Equal(expected, result)
 
   [<Theory>]
