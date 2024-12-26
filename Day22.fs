49c03944dbd060363e1f2b34d05ae1b9762e82e9
Justin LeFebvre <jstnlefebvre@gmail.com>
2024-12-26 13:37:04 -0500

    day 22 part 1

--- Day22.fs
+++ Day22.fs
@@ -1,13 +1,28 @@
 module Day22
 
+open System.IO
+
+let secretNumber n initial =
+  let generateNext secret =
+    let mutable s = secret
+    s <- ((s * 64L) ^^^ s) % 16777216L
+    s <- ((s / 32L) ^^^ s) % 16777216L
+    s <- ((s * 2048L) ^^^ s) % 16777216L
+    s
+
+  seq { 1..n } |> Seq.fold (fun s _ -> generateNext s) initial
+
+let parse filename =
+  filename |> File.ReadAllLines |> Array.map int64
+
 module Tests =
   open Xunit
 
   [<Theory>]
-  [<InlineData("Inputs/Day22/test.txt", -1)>]
-  [<InlineData("Inputs/Day22/input.txt", -1)>]
-  let ``Part 1`` (filename: string, expected: int) =
-    let result = 0
+  [<InlineData("Inputs/Day22/test.txt", 37327623)>]
+  [<InlineData("Inputs/Day22/input.txt", 21147129593L)>]
+  let ``Part 1`` (filename: string, expected: int64) =
+    let result = filename |> parse |> Array.sumBy (secretNumber 2000)
     Assert.Equal(expected, result)
 
   [<Theory>]
